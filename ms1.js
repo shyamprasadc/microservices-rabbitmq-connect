9e13c30bf099198ff4fce54868601fd34cfb177e
Shyam Prasad <shyam@accubits.com>
2021-04-02 22:23:12 +0530

    first commit

---
+++ ms1.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const PORT = 5001;
+const amqp = require("amqplib");
+var channel, connection;
+
+app.use(express.json());
+
+(async function connect() {
+  try {
+    const amqpServer = "amqp://localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("session");
+  } catch (ex) {
+    console.error(ex);
+  }
+})();
+
+const createSession = async (user) => {
+  await channel.sendToQueue("session", Buffer.from(JSON.stringify(user)));
+  // await channel.close();
+  // await connection.close();
+};
+
+app.post("/login", (req, res) => {
+  const user = { email: req.body.email, password: req.body.password };
+  createSession(user);
+  res.send(user);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server at ${PORT}`);
+});
